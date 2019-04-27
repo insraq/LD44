b4fcce906e15d008315125d851bee951edd98219
Ruoyu Sun <ruoysun@gmail.com>
2019-04-28 01:01:55 +0300

    Animation

--- assets/scripts/Card.ts
+++ assets/scripts/Card.ts
@@ -1,9 +1,22 @@
 import { CardEntity } from "./CardData";
+import CardDeck from "./CardDeck";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Card extends cc.Component {
+    public get level(): number {
+        return this._level;
+    }
+
+    public set level(value: number) {
+        this._level = value;
+        this.level1.active = this._level >= 1;
+        this.level2.active = this._level >= 2;
+        this.level3.active = this._level >= 3;
+    }
+
+    public card: CardEntity = null;
 
     @property(cc.Node)
     private level1: cc.Node = null;
@@ -15,22 +28,9 @@ export default class Card extends cc.Component {
     private pattern: cc.Sprite = null;
     @property(cc.Node)
     private tileFrame: cc.Node = null;
-
-    private card: CardEntity = null;
-
-    private $CardName: string;
+    private previousPosition: cc.Vec2 = null;
 
     private _level: number;
-    public get level(): number {
-        return this._level;
-    }
-
-    public set level(value: number) {
-        this._level = value;
-        this.level1.active = this._level >= 1;
-        this.level2.active = this._level >= 2;
-        this.level3.active = this._level >= 3;
-    }
 
     public build(card: CardEntity) {
         this.card = card;
@@ -50,9 +50,23 @@ export default class Card extends cc.Component {
         this.tileFrame.color = color;
     }
 
-    protected onLoad() {
+    protected start() {
+        this.node.on(cc.Node.EventType.TOUCH_START, (event: cc.Event.EventTouch) => {
+            this.previousPosition = this.node.position;
+        });
         this.node.on(cc.Node.EventType.TOUCH_MOVE, (event: cc.Event.EventTouch) => {
-            this.node.position = this.node.parent.convertTouchToNodeSpace(event.touch);
+            this.node.position = this.node.parent.convertToNodeSpaceAR(event.getLocation());
+        });
+        this.node.on(cc.Node.EventType.TOUCH_END, (event: cc.Event.EventTouch) => {
+            if (this.node.position.y > this.previousPosition.y) {
+                if (!CardDeck.getInstance().moveToSquad(this)) {
+                    this.node.position = this.previousPosition;
+                }
+            } else {
+                if (!CardDeck.getInstance().moveToHolding(this)) {
+                    this.node.position = this.previousPosition;
+                }
+            }
         });
     }
 
--- assets/scripts/CardDeck.ts
+++ assets/scripts/CardDeck.ts
@@ -3,38 +3,111 @@ import { CardEntity, Color, Pattern } from "./CardData";
 
 const { ccclass, property } = cc._decorator;
 
-declare const vConsole: any;
+const PADDING = 60;
+const NODE_WIDTH = 150;
+const ANIMATION_TIME = 0.25;
+const MAX_HOLDING = 6;
+
+let _instance: CardDeck = null;
 
 @ccclass
 export default class CardDeck extends cc.Component {
 
-    @property(cc.Prefab)
-    private cardPrefab: cc.Prefab = null;
-
-    protected onLoad() {
-        const cards = [
-            new CardEntity(Color.Red, Pattern.Tree3),
-            new CardEntity(Color.Purple, Pattern.Tree2),
-            new CardEntity(Color.Blue, Pattern.Tree1),
-            new CardEntity(Color.Green, Pattern.Tree1),
-            new CardEntity(Color.Green, Pattern.Tree2),
-            new CardEntity(Color.Green, Pattern.Tree3),
-        ];
-        renderCards(cards, this.cardPrefab, this.node);
+    public static getInstance(): CardDeck {
+        if (_instance === null) {
+            throw new Error("Placeholders is null, likely due to onLoad execution order");
+        }
+        return _instance;
     }
 
-}
+    public readonly holding: CardEntity[] = [];
+    public readonly squad: CardEntity[] = [];
 
-export function renderCards(cards: CardEntity[], cardPrefab: cc.Prefab, renderTarget: cc.Node) {
-    const padding = 60;
-    const w = cc.winSize.width - padding;
-    cards.forEach((card, i) => {
-        const node = cc.instantiate(cardPrefab);
-        const dist = (w - cards.length * node.width) / (cards.length - 1);
-        const c = node.getComponent(Card);
-        c.build(card);
-        node.parent = renderTarget;
-        node.x = padding / 2 + (dist + node.width) * i + node.width / 2;
-        return node;
-    });
+    private _unlockedSquad: number = 0;
+    public get unlockedSquad() {
+        return this._unlockedSquad;
+    }
+    public set unlockedSquad(value) {
+        this.placeholders.children.forEach((node, i) => {
+            node.children[0].active = i >= value;
+        });
+        this._unlockedSquad = value;
+    }
+
+    @property(cc.Prefab)
+    private cardPrefab: cc.Prefab = null;
+    @property(cc.Node)
+    private placeholders: cc.Node = null;
+    @property(cc.Node)
+    private holdingNode: cc.Node = null;
+    @property(cc.Node)
+    private squadNode: cc.Node = null;
+
+    public moveToSquad(c: Card): boolean {
+        const index = this.holding.indexOf(c.card);
+        if (index === -1 || this.squad.length >= this._unlockedSquad) {
+            return false;
+        }
+        const cards = this.holding.splice(index, 1);
+        this.squad.push(cards[0]);
+
+        c.node.parent = this.squadNode;
+        c.node.position = this.squadNode.convertToNodeSpaceAR(this.holdingNode.convertToWorldSpaceAR(c.node.position));
+        c.node.runAction(cc.moveTo(ANIMATION_TIME, this.placeholders.children[this.squad.length - 1].position));
+
+        this.rearrangeHolding();
+        return true;
+    }
+
+    public moveToHolding(c: Card): boolean {
+        const index = this.squad.indexOf(c.card);
+        if (index === -1 || this.holding.length >= MAX_HOLDING) {
+            return false;
+        }
+
+        const cards = this.squad.splice(index, 1);
+        this.holding.push(cards[0]);
+
+        c.node.parent = this.holdingNode;
+        c.node.position = this.holdingNode.convertToNodeSpaceAR(this.squadNode.convertToWorldSpaceAR(c.node.position));
+        this.rearrangeHolding();
+        return true;
+    }
+
+    protected onLoad() {
+        _instance = this;
+    }
+
+    protected start() {
+        this.holding.push(new CardEntity(Color.Red, Pattern.Tree3));
+        this.holding.push(new CardEntity(Color.Black, Pattern.Tree2));
+        this.holding.push(new CardEntity(Color.Blue, Pattern.Tree1));
+        this.holding.push(new CardEntity(Color.Red, Pattern.Tree2));
+        this.holding.push(new CardEntity(Color.Green, Pattern.Tree3));
+        this.holding.push(new CardEntity(Color.Purple, Pattern.Tree1));
+
+        this.unlockedSquad = 2;
+
+        this.holding.forEach((card) => {
+            const node = cc.instantiate(this.cardPrefab);
+            const c = node.getComponent(Card);
+            c.build(card);
+            node.parent = this.holdingNode;
+        });
+
+        this.rearrangeHolding();
+    }
+
+    private rearrangeHolding() {
+        this.holdingNode.children.forEach((card, i) => {
+            card.runAction(cc.moveTo(ANIMATION_TIME, this.getCardX(i), 0));
+        });
+    }
+
+    private getCardX(index: number) {
+        const w = cc.winSize.width - PADDING;
+        const dist = (w - this.holding.length * NODE_WIDTH) / (this.holding.length - 1);
+        return PADDING / 2 + (dist + NODE_WIDTH) * index + NODE_WIDTH / 2;
+    }
+
 }
